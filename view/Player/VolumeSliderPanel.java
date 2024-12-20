2e2335485054cdbecc4398a8ef1c155037bff5fd
Rahul kumar  /* A J R */ <rahulkumar108642@gmail.com>
2024-12-20 12:59:55 +0530

    upload for ressumission

---
+++ view/Player/VolumeSliderPanel.java
@@ -0,0 +1,33 @@
+package view.Player;
+
+import controller.AudioController;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+
+/**
+ * this class increase and decreases volume of the song using a slider on the right side of the panel
+ */
+public class VolumeSliderPanel extends JPanel{
+
+    private JSlider slider;
+
+    public VolumeSliderPanel(){
+
+        this.setBackground(new Color(40,40,40));
+        slider = new JSlider();
+        slider.setMinimum(0);
+        slider.setMaximum(100);
+        slider.setToolTipText("Volume");
+        slider.setPreferredSize(new Dimension(130,25));
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                AudioController.setMasterOutputVolume((float)slider.getValue()/100);
+            }
+        });
+        add(slider);
+    }
+}
